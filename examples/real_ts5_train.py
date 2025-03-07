7ad0753c115a8d0e8cf89848e2dae51517e7fe52
zhanwenxin22 <zhanwen.w.xin@gmail.com>
2025-03-07 11:59:24 -0500

    initialize hsl_detection class

--- examples/real_ts5_train.py
+++ examples/real_ts5_train.py
@@ -21,6 +21,7 @@ from pytagi import exponential_scheduler
 import pytagi.metric as metric
 from pytagi import Normalizer as normalizer
 from matplotlib import gridspec
+import pickle
 
 
 # # Read data
@@ -116,78 +117,13 @@ print(f"Validation MSE      :{model.early_stop_metric: 0.4f}")
 
 model_dict = model.save_model_dict()
 model_dict['states_optimal'] = states_optim
+model_dict['early_stop_init_mu_states'] = model.early_stop_init_mu_states
+model_dict['early_stop_init_var_states'] = model.early_stop_init_var_states
 
-####################################################################
-######################### Pretrained model #########################
-####################################################################
-print("phi_AR =", model_dict['states_optimal'].mu_prior[-1][model_dict['phi_index']].item())
-print("sigma_AR =", np.sqrt(model_dict['states_optimal'].mu_prior[-1][model_dict['W2bar_index']].item()))
-pretrained_model = Model(
-    # LocalTrend(mu_states=model_dict["mu_states"][0:2].reshape(-1), var_states=np.diag(model_dict["var_states"][0:2, 0:2])),
-    LocalTrend(mu_states=model_dict["mu_states"][0:2].reshape(-1), var_states=[1e-12, 1e-12]),
-    LSTM,
-    Autoregression(std_error=np.sqrt(model_dict['states_optimal'].mu_prior[-1][model_dict['W2bar_index']].item()), 
-                   phi=model_dict['states_optimal'].mu_prior[-1][model_dict['phi_index']].item(), 
-                   mu_states=[model_dict["mu_states"][model_dict['autoregression_index']].item()], 
-                   var_states=[model_dict["var_states"][model_dict['autoregression_index'], model_dict['autoregression_index']].item()]),
-)
-
-pretrained_model.lstm_net.load_state_dict(model.lstm_net.state_dict())
-
-pretrained_model.filter(normalized_data,train_lstm=False)
-pretrained_model.smoother(normalized_data)
-
-#  Plot
-state_type = "prior"
-#  Plot states from pretrained model
-fig = plt.figure(figsize=(10, 6))
-gs = gridspec.GridSpec(4, 1)
-ax0 = plt.subplot(gs[0])
-ax1 = plt.subplot(gs[1])
-ax2 = plt.subplot(gs[2])
-ax3 = plt.subplot(gs[3])
-plot_data(
-    data_processor=data_processor,
-    normalization=True,
-    plot_column=output_col,
-    validation_label="y",
-    sub_plot=ax0,
-)
-plot_states(
-    data_processor=data_processor,
-    states=pretrained_model.states,
-    states_type=state_type,
-    states_to_plot=['local level'],
-    sub_plot=ax0,
-)
-ax0.set_xticklabels([])
-ax0.set_title("Hidden states estimated by the pre-trained model")
-plot_states(
-    data_processor=data_processor,
-    states=pretrained_model.states,
-    states_type=state_type,
-    states_to_plot=['local trend'],
-    sub_plot=ax1,
-)
-ax1.set_xticklabels([])
-plot_states(
-    data_processor=data_processor,
-    states=pretrained_model.states,
-    states_type=state_type,
-    states_to_plot=['lstm'],
-    sub_plot=ax2,
-)
-ax2.set_xticklabels([])
-plot_states(
-    data_processor=data_processor,
-    states=pretrained_model.states,
-    states_type=state_type,
-    states_to_plot=['autoregression'],
-    sub_plot=ax3,
-)
-ax3.set_xticklabels([])
-# plt.show()
-
+# # Save model_dict to local
+# import pickle
+# with open("saved_params/real_ts5_model.pkl", "wb") as f:
+#     pickle.dump(model_dict, f)
 
 state_type = "prior"
 # Plot states from AR learner
