b98c38404e6c5feeaa4b52898ad4b1e5d5513c04
zhanwenxin22 <zhanwen.w.xin@gmail.com>
2025-03-12 11:17:51 -0400

    ready to generate NN samples

--- examples/real_ts5_tsad.py
+++ examples/real_ts5_tsad.py
@@ -94,7 +94,7 @@ hsl_tsad_agent.drift_model.var_states = hsl_tsad_agent_pre.drift_model.var_state
 mu_obs_preds, std_obs_preds, mu_ar_preds, std_ar_preds = hsl_tsad_agent.filter(train_data, buffer_LTd=True)
 mu_obs_preds, std_obs_preds, mu_ar_preds, std_ar_preds = hsl_tsad_agent.filter(validation_data, buffer_LTd=True)
 hsl_tsad_agent.estimate_LTd_dist()
-hsl_tsad_agent.collect_synthetic_samples()
+hsl_tsad_agent.collect_synthetic_samples(num_time_series=1000)
 mu_obs_preds, std_obs_preds, mu_ar_preds, std_ar_preds = hsl_tsad_agent.detect(test_data)
 
 #  Plot
