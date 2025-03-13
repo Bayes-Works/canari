5a045665f554e2b5ec8a2067ce3649b46ef281bb
zhanwenxin22 <zhanwen.w.xin@gmail.com>
2025-03-13 11:53:12 -0400

    not working well o simple synthetic time series

---
+++ examples/toy_simple_tsad.py
@@ -0,0 +1,199 @@
+import fire
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+from src import (
+    LocalLevel,
+    LocalTrend,
+    LocalAcceleration,
+    LstmNetwork,
+    Periodic,
+    Autoregression,
+    WhiteNoise,
+    Model,
+    plot_data,
+    plot_prediction,
+    plot_states,
+)
+from src.hsl_detection import hsl_detection
+from examples import DataProcess
+from pytagi import exponential_scheduler
+import pytagi.metric as metric
+from pytagi import Normalizer as normalizer
+from matplotlib import gridspec
+import pickle
+from src.model import load_model_dict
+
+
+# # Read data
+data_file = "./data/toy_time_series/synthetic_simple_autoregression_periodic_2.csv"
+df_raw = pd.read_csv(data_file, skiprows=1, delimiter=",", header=None)
+linear_space = np.linspace(0, 3, num=len(df_raw))
+# Set the first 52*12 values in linear_space to be 0
+anm_start_index = 52*12
+linear_space[anm_start_index:] -= linear_space[anm_start_index]
+linear_space[:anm_start_index] = 0
+
+df_raw = df_raw.add(linear_space, axis=0)
+
+data_file_time = "./data/toy_time_series/synthetic_simple_autoregression_periodic_datetime.csv"
+time_series = pd.read_csv(data_file_time, skiprows=1, delimiter=",", header=None)
+time_series = pd.to_datetime(time_series[0])
+df_raw.index = time_series
+df_raw.index.name = "date_time"
+df_raw.columns = ["values"]
+
+# Data pre-processing
+output_col = [0]
+train_split=0.289
+validation_split=0.0693*2
+data_processor = DataProcess(
+    data=df_raw,
+    time_covariates=["week_of_year"],
+    train_split=train_split,
+    validation_split=validation_split,
+    output_col=output_col,
+)
+
+train_data, validation_data, test_data, normalized_data = data_processor.get_splits()
+
+
+####################################################################
+######################### Pretrained model #########################
+####################################################################
+# Load model_dict from local
+with open("saved_params/toy_simple_model.pkl", "rb") as f:
+    model_dict = pickle.load(f)
+
+LSTM = LstmNetwork(
+        look_back_len=19,
+        num_features=2,
+        num_layer=1,
+        num_hidden_unit=50,
+        device="cpu",
+    )
+
+print("phi_AR =", model_dict['states_optimal'].mu_prior[-1][model_dict['phi_index']].item())
+print("sigma_AR =", np.sqrt(model_dict['states_optimal'].mu_prior[-1][model_dict['W2bar_index']].item()))
+
+
+pretrained_model = Model(
+    LocalTrend(mu_states=model_dict['early_stop_init_mu_states'][0:2].reshape(-1), var_states=[1e-12, 1e-12]),
+    LSTM,
+    Autoregression(std_error=np.sqrt(model_dict['states_optimal'].mu_prior[-1][model_dict['W2bar_index']].item()), 
+                   phi=model_dict['states_optimal'].mu_prior[-1][model_dict['phi_index']].item(), 
+                   mu_states=[model_dict['early_stop_init_mu_states'][model_dict['autoregression_index']].item()], 
+                   var_states=[model_dict['early_stop_init_var_states'][model_dict['autoregression_index'], model_dict['autoregression_index']].item()]),
+)
+
+pretrained_model.lstm_net.load_state_dict(model_dict["lstm_network_params"])
+
+ltd_error = 1e-5
+
+hsl_tsad_agent = hsl_detection(base_model=pretrained_model, data_processor=data_processor, drift_model_process_error_std=ltd_error)
+
+# Get flexible drift model from the beginning
+hsl_tsad_agent_pre = hsl_detection(base_model=load_model_dict(pretrained_model.save_model_dict()), data_processor=data_processor, drift_model_process_error_std=ltd_error)
+hsl_tsad_agent_pre.filter(train_data)
+hsl_tsad_agent_pre.filter(validation_data)
+hsl_tsad_agent.drift_model.var_states = hsl_tsad_agent_pre.drift_model.var_states
+
+
+mu_obs_preds, std_obs_preds, mu_ar_preds, std_ar_preds = hsl_tsad_agent.filter(train_data, buffer_LTd=True)
+mu_obs_preds, std_obs_preds, mu_ar_preds, std_ar_preds = hsl_tsad_agent.filter(validation_data, buffer_LTd=True)
+hsl_tsad_agent.estimate_LTd_dist()
+# hsl_tsad_agent.collect_synthetic_samples(num_time_series=300)
+# hsl_tsad_agent.learn_intervention(training_samples_path='data/hsl_tsad_training_samples/hsl_tsad_train_samples.csv', 
+#                                   save_model_path='saved_params/NN_detection_model.pkl')
+hsl_tsad_agent.learn_intervention(training_samples_path='data/hsl_tsad_training_samples/hsl_tsad_train_samples.csv', 
+                                  load_model_path='saved_params/NN_detection_model_process_error_e5.pkl')
+mu_obs_preds, std_obs_preds, mu_ar_preds, std_ar_preds = hsl_tsad_agent.detect(test_data, apply_intervention=True)
+
+#  Plot
+state_type = "prior"
+#  Plot states from pretrained model
+fig = plt.figure(figsize=(10, 8))
+gs = gridspec.GridSpec(8, 1)
+ax0 = plt.subplot(gs[0])
+ax1 = plt.subplot(gs[1])
+ax2 = plt.subplot(gs[2])
+ax3 = plt.subplot(gs[3])
+ax4 = plt.subplot(gs[4])
+ax5 = plt.subplot(gs[5])
+ax6 = plt.subplot(gs[6])
+ax7 = plt.subplot(gs[7])
+from src.data_visualization import determine_time
+time = determine_time(data_processor, len(normalized_data["y"]))
+plot_data(
+    data_processor=data_processor,
+    normalization=True,
+    plot_column=output_col,
+    validation_label="y",
+    sub_plot=ax0,
+)
+plot_states(
+    data_processor=data_processor,
+    # states=pretrained_model.states,
+    states=hsl_tsad_agent.base_model.states,
+    states_type=state_type,
+    states_to_plot=['local level'],
+    sub_plot=ax0,
+)
+ax0.axvline(x=time[anm_start_index], color='r', linestyle='--')
+ax0.set_xticklabels([])
+ax0.set_title("Hidden states likelihood")
+plot_states(
+    data_processor=data_processor,
+    states=hsl_tsad_agent.base_model.states,
+    states_type=state_type,
+    states_to_plot=['local trend'],
+    sub_plot=ax1,
+)
+ax1.set_xticklabels([])
+plot_states(
+    data_processor=data_processor,
+    states=hsl_tsad_agent.base_model.states,
+    states_type=state_type,
+    states_to_plot=['lstm'],
+    sub_plot=ax2,
+)
+ax2.set_xticklabels([])
+plot_states(
+    data_processor=data_processor,
+    states=hsl_tsad_agent.base_model.states,
+    states_type=state_type,
+    states_to_plot=['autoregression'],
+    sub_plot=ax3,
+)
+ax3.set_xticklabels([])
+plot_states(
+    data_processor=data_processor,
+    states=hsl_tsad_agent.drift_model.states,
+    states_type=state_type,
+    states_to_plot=['local level'],
+    sub_plot=ax4,
+)
+ax4.set_xticklabels([])
+plot_states(
+    data_processor=data_processor,
+    states=hsl_tsad_agent.drift_model.states,
+    states_type=state_type,
+    states_to_plot=['local trend'],
+    sub_plot=ax5,
+)
+ax5.set_xticklabels([])
+plot_states(
+    data_processor=data_processor,
+    states=hsl_tsad_agent.drift_model.states,
+    states_type=state_type,
+    states_to_plot=['autoregression'],
+    sub_plot=ax6,
+)
+ax6.set_xticklabels([])
+ax7.plot(time, hsl_tsad_agent.p_anm_all)
+ax7.set_ylabel("p_anm")
+ax7.set_xlim(ax0.get_xlim())
+ax7.axvline(x=time[anm_start_index], color='r', linestyle='--')
+ax7.set_ylim(-0.05, 1.05)
+plt.show()
