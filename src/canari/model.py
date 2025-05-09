9c4934a880fb2c4614d3131efefc96eade0b6a27
David <david.wardan@polymtl.ca>
2025-05-09 16:26:38 -0400

    version with bugs

--- src/canari/model.py
+++ src/canari/model.py
@@ -30,6 +30,7 @@ import canari.common as common
 from canari.data_struct import LstmOutputHistory, StatesHistory
 from canari.common import GMA
 from canari.data_process import DataProcess
+from pytagi.nn import OutputUpdater
 from pytagi import Normalizer as normalizer
 
 
@@ -977,11 +978,22 @@ class Model:
 
         mu_obs_preds = []
         std_obs_preds = []
+        if self.lstm_net.smooth:
+            out_updater = OutputUpdater(self.lstm_net.device)
 
         for x in data["x"]:
             mu_obs_pred, var_obs_pred, mu_states_prior, var_states_prior = self.forward(
                 x
             )
+            if self.lstm_net.smooth:
+                out_updater.update(
+                    output_states=self.lstm_net.output_z_buffer,
+                    mu_obs=np.array([np.nan], dtype=np.float32),
+                    var_obs=np.array([0], dtype=np.float32),
+                    delta_states=self.lstm_net.input_delta_z_buffer,
+                )
+                self.lstm_net.backward()
+                self.lstm_net.step()
 
             if self.lstm_net:
                 lstm_index = self.get_states_index("lstm")
@@ -1152,9 +1164,40 @@ class Model:
                 self._current_epoch, white_noise_max_std, white_noise_decay_factor
             )
         self.filter(train_data)
-        self.smoother()
+        self.smoother() #TODO: why not train_data
+
+        mu_validation_preds = np.zeros(len(validation_data["y"]))
+        std_validation_preds = np.zeros(len(validation_data["y"]))
+
+        # if self.lstm_net.smooth:
+        #     mu_zo_smooth, var_zo_smooth = self.lstm_net.smoother()
+        #     zo_smooth_std = np.array(var_zo_smooth) ** 0.5
+        #     mu_sequence = mu_zo_smooth[: self.lstm_net.lstm_look_back_len]
+        #     var_sequence = var_zo_smooth[: self.lstm_net.lstm_look_back_len]
+        #     self.lstm_output_history.mu = mu_sequence
+        #     self.lstm_output_history.var = var_sequence
+        #     self.initialize_states_with_smoother_estimates()
+        # else:
+        #     self.set_memory(states=self.states, time_step=0)
+
+        # return (
+        #     np.array(mu_validation_preds).flatten(),
+        #     np.array(std_validation_preds).flatten(),
+        #     self.states,
+        # )
+
         mu_validation_preds, std_validation_preds, _ = self.forecast(validation_data)
-        # self.set_memory(states=self.states, time_step=0)
+
+        if self.lstm_net.smooth:
+            mu_zo_smooth, var_zo_smooth = self.lstm_net.smoother()
+            zo_smooth_std = np.array(var_zo_smooth) ** 0.5
+            mu_sequence = mu_zo_smooth[: self.lstm_net.lstm_look_back_len]
+            var_sequence = var_zo_smooth[: self.lstm_net.lstm_look_back_len]
+            self.lstm_output_history.mu = mu_sequence
+            self.lstm_output_history.var = var_sequence
+            self.initialize_states_with_smoother_estimates()
+        # else:
+        #     self.set_memory(states=self.states, time_step=0)
 
         self._current_epoch += 1
 
