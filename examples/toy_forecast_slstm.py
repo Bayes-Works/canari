c2ee4d5c04d44103d54d12d3aee64cb6c113de83
David <david.wardan@polymtl.ca>
2025-05-14 16:41:51 -0400

    working version

--- examples/toy_forecast_slstm.py
+++ examples/toy_forecast_slstm.py
@@ -11,8 +11,8 @@ from canari.component import LstmNetwork, WhiteNoise, LocalTrend
 # # Read data
 data_file = "./data/toy_time_series/sine.csv"
 df_raw = pd.read_csv(data_file, skiprows=1, delimiter=",", header=None)
-# linear_space = np.linspace(0, 2, num=len(df_raw))
-# df_raw = df_raw.add(linear_space, axis=0)
+linear_space = np.linspace(0, 2, num=len(df_raw))
+df_raw = df_raw.add(linear_space, axis=0)
 
 data_file_time = "./data/toy_time_series/sine_datetime.csv"
 time_series = pd.read_csv(data_file_time, skiprows=1, delimiter=",", header=None)
@@ -26,7 +26,7 @@ df = df_raw.resample("H").mean()
 
 # Define parameters
 output_col = [0]
-num_epoch = 50
+num_epoch = 10
 
 data_processor = DataProcess(
     data=df,
@@ -38,14 +38,14 @@ data_processor = DataProcess(
 train_data, validation_data, test_data, normalized_data = data_processor.get_splits()
 
 # Model
-sigma_v = 0.0032322250444898116
+sigma_v = 0.001
 model = Model(
-    # LocalTrend(),
+    LocalTrend(),
     LstmNetwork(
-        look_back_len=19,
+        look_back_len=12,
         num_features=1,
         num_layer=1,
-        num_hidden_unit=50,
+        num_hidden_unit=40,
         device="cpu",
         manual_seed=1,
         smoother=True,
@@ -53,71 +53,37 @@ model = Model(
     WhiteNoise(std_error=sigma_v),
 )
 model.auto_initialize_baseline_states(train_data["y"][0:24])
-# model.lstm_net.num_samples = model.lstm_net.lstm_look_back_len + len(train_data["y"])
-model.lstm_net.num_samples = (
-    model.lstm_net.lstm_look_back_len + len(train_data["y"]) + len(validation_data["y"])
-)
+
+if model.lstm_net.smooth:
+    model.lstm_net.num_samples = (
+        # model.lstm_net.lstm_look_back_len
+        +len(train_data["y"])
+        # + len(validation_data["y"])
+    )
 
 # Training
 for epoch in range(num_epoch):
-    (mu_validation_preds, std_validation_preds, states) = model.lstm_train(
-        train_data=train_data,
-        validation_data=validation_data,
-    )
 
-    # Unstandardize the predictions
-    mu_validation_preds = normalizer.unstandardize(
-        mu_validation_preds,
-        data_processor.norm_const_mean[output_col],
-        data_processor.norm_const_std[output_col],
-    )
-    std_validation_preds = normalizer.unstandardize_std(
-        std_validation_preds,
-        data_processor.norm_const_std[output_col],
-    )
+    # set white noise decay
+    model._white_noise_decay(epoch, white_noise_max_std=3, white_noise_decay_factor=0.9)
 
-    # Calculate the log-likelihood metric
-    validation_obs = data_processor.get_data("validation").flatten()
-    mse = metric.mse(mu_validation_preds, validation_obs)
+    # filter on train data
+    mu_preds_train, std_preds_train, states = model.filter(train_data, train_lstm=True)
+
+    # smooth on train data
+    model.smoother()
+
+    # smooth lstm states
+    if model.lstm_net.smooth:
+        mu_zo_smooth, var_zo_smooth = model.lstm_net.smoother()
 
     fig, ax = plot_states(
         data_processor=data_processor,
-        states=states,
+        states=model.states,
         states_type="prior",
     )
     filename = f"saved_results/smoother#{epoch}.png"
     plt.savefig(filename)
     plt.close()
 
-    # Early-stopping
-    model.early_stopping(evaluate_metric=mse, mode="min")
-    if epoch == model.optimal_epoch:
-        mu_validation_preds_optim = mu_validation_preds
-        std_validation_preds_optim = std_validation_preds
-        states_optim = copy.copy(
-            states
-        )  # If we want to plot the states, plot those from optimal epoch
-    if model.stop_training:
-        break
-    else:
-        model.set_memory(states=states, time_step=0)
-
-print(f"Optimal epoch       : {model.optimal_epoch}")
-print(f"Validation MSE      :{model.early_stop_metric: 0.4f}")
-
-#  Plot
-fig, ax = plt.subplots(figsize=(10, 6))
-plot_data(
-    data_processor=data_processor,
-    normalization=False,
-    plot_column=output_col,
-    validation_label="y",
-)
-plot_prediction(
-    data_processor=data_processor,
-    mean_validation_pred=mu_validation_preds,
-    std_validation_pred=std_validation_preds,
-    validation_label=[r"$\mu$", f"$\pm\sigma$"],
-)
-plt.legend()
-plt.show()
+    model.set_memory(states=model.states, time_step=0)
