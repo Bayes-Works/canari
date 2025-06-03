d0e1e86c919ae458a54b88fff83176bdabb8f1dd
David <david.wardan@polymtl.ca>
2025-06-03 16:23:01 -0400

    Refactor LSTM model initialization and training process; adjust parameters and remove unused code

--- src/canari/component/lstm_component.py
+++ src/canari/component/lstm_component.py
@@ -206,6 +206,7 @@ class LstmNetwork(BaseComponent):
         # Initialize lstm network
         lstm_network = Sequential(*layers)
         lstm_network.lstm_look_back_len = self.look_back_len
+        lstm_network.num_covariates = self.num_features - 1
         if self.device == "cpu":
             lstm_network.set_threads(self.num_thread)
         elif self.device == "cuda":
