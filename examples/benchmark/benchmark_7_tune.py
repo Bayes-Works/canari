cac2b59b3d87353a03a101fb74dd3d0e8dc5c3cf
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2025-06-09 11:29:17 -0400

    smoother add tolerance

--- examples/benchmark/benchmark_7_tune.py
+++ examples/benchmark/benchmark_7_tune.py
@@ -292,13 +292,13 @@ def main(
 
     # Detect anomaly
     filter_marginal_abnorm_prob, states = skf_optim.filter(data=all_data)
-    smooth_marginal_abnorm_prob, states = skf_optim.smoother()
+    smooth_marginal_abnorm_prob, states = skf_optim.smoother(matrix_inversion_tol=1e-4)
 
     fig, ax = plot_skf_states(
         data_processor=data_processor,
         states=states,
         states_type="smooth",
-        states_to_plot=["level", "trend", "lstm", "white noise"],
+        # states_to_plot=["level", "trend", "lstm", "white noise"],
         model_prob=filter_marginal_abnorm_prob,
     )
     fig.suptitle("SKF hidden states", fontsize=10, y=1)
