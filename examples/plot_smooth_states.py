cf64fc2703644b7a32de6a581707404decbfdf6b
David <david.wardan@polymtl.ca>
2025-06-10 14:38:44 -0400

    adding covariates to the first look_back

--- examples/plot_smooth_states.py
+++ examples/plot_smooth_states.py
@@ -5,7 +5,7 @@ from canari import DataProcess
 from pytagi import Normalizer as normalizer
 
 
-def main(L=24):
+def main(L=24, observations=None, epoch=0):
     # set lookback
 
     # --- 1. Load the raw CSV (no header) ---
@@ -18,32 +18,6 @@ def main(L=24):
     # --- 3. Filter for only the 'mu' variables (priors, posts, smooths) ---
     mu_df = df_raw[df_raw[1].str.startswith("mu")]
     var_df = df_raw[df_raw[1].str.startswith("var")]
-
-    # read training data
-    data_file = "./data/toy_time_series/sine.csv"
-    df_raw = pd.read_csv(data_file, skiprows=1, delimiter=",", header=None)
-
-    data_file_time = "./data/toy_time_series/sine_datetime.csv"
-    time_series = pd.read_csv(data_file_time, skiprows=1, delimiter=",", header=None)
-    time_series = pd.to_datetime(time_series[0])
-    df_raw.index = time_series
-    df_raw.index.name = "date_time"
-    df_raw.columns = ["values"]
-
-    # Resampling data
-    df = df_raw.resample("H").mean()
-    data_processor = DataProcess(
-        data=df,
-        train_split=0.8,
-        validation_split=0.1,
-        output_col=[0],
-    )
-    train_data = (
-        df["values"]
-        .iloc[data_processor.train_start : data_processor.train_end]
-        .to_list()
-    )
-    print(len(train_data))
 
     # --- 4. Plot ---
     plt.figure(figsize=(8, 4))
@@ -63,7 +37,7 @@ def main(L=24):
     # add real data
     plt.plot(
         time_steps[L:],
-        train_data,
+        observations,
         color="red",
         label="y",
     )
@@ -74,7 +48,8 @@ def main(L=24):
     plt.legend(loc=(0, 1.01), ncol=4)
     plt.grid(True)
     plt.tight_layout()
-    plt.show()
+    plt.savefig("./saved_results/smooth_states_plot_{}.png".format(epoch), dpi=300)
+    plt.close()
 
 
 if __name__ == "__main__":
