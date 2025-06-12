d2fbe57f2775e4bd375809fe4b09033745508272
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2025-06-12 12:33:23 -0400

    unit test use model functions

--- src/canari/skf.py
+++ src/canari/skf.py
@@ -939,6 +939,8 @@ class SKF:
             ) = transition_model.forward(
                 mu_lstm_pred=mu_lstm_pred, var_lstm_pred=var_lstm_pred
             )
+            # if np.isnan(mu_pred_transit[transit]):
+            #     check = 1
 
         self.transition_coef = self._estimate_transition_coef(
             obs,
