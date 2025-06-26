6362ad075ac87054981af0572e9a3a7f52019ebe
zhanwenxin22 <zhanwen.w.xin@gmail.com>
2025-06-26 12:29:28 -0400

    comparison of skf and IL under different ssm quality

--- examples/prob_eva_detrended_ts5_skf.py
+++ examples/prob_eva_detrended_ts5_skf.py
@@ -47,6 +47,7 @@ test_start = df_raw.index[data_processor.test_start]
 ####################################################################
 # Load model_dict from local
 with open("saved_params/real_ts5_detrend_tsmodel_better.pkl", "rb") as f:
+# with open("saved_params/real_ts5_detrend_tsmodel.pkl", "rb") as f:
     model_dict = pickle.load(f)
 
 LSTM = LstmNetwork(
@@ -65,7 +66,6 @@ print("phi_AR =", model_dict['states_optimal'].mu_prior[-1][phi_index].item())
 print("sigma_AR =", np.sqrt(model_dict['states_optimal'].mu_prior[-1][W2bar_index].item()))
 
 
-# No detection: 3, 7
 # std_trans_error_norm_to_abnorm_prob_combs = [[0.00017265262887754653, 7.171842166745157e-06],
 #                                              [0.00016094645369552546, 2.4004806216088062e-06],
 #                                              [0.0003520899194267602, 7.075851051622472e-06],
@@ -91,10 +91,78 @@ print("sigma_AR =", np.sqrt(model_dict['states_optimal'].mu_prior[-1][W2bar_inde
 #                                      ]
 
 # Tuned models params for better model
-stdtrans_normtoab_probthred_combs = [[0.00023339656126145536, 3.705613810296851e-05, 0.01944423014032889],
-                                     [0.00011559975847875379, 3.3680733434277974e-05, 0.010182633021558266],
-                                     [0.0002634174902743065, 9.922253793029194e-06, 0.04417427865718894],
+stdtrans_normtoab_probthred_combs = [
+                                    #  [0.00023339656126145536, 3.705613810296851e-05, 0.01944423014032889],
+                                    #  [0.00011559975847875379, 3.3680733434277974e-05, 0.010182633021558266],
+                                    #  [0.0002634174902743065, 9.922253793029194e-06, 0.04417427865718894],
+                                    #  [0.0001999371854360042, 7.436014404637097e-06, 0.03523428875043497],
+                                    #  [0.0009636388679487375, 2.3905423903777268e-06, 0.010270388206854823],
+                                     [0.00012170458965627041, 2.6079044862012114e-05, 0.18508348377123054],
+                                     [0.00010554161018347391, 0.000163181236096615, 0.07372733495694839],
+                                     [6.86255250089033e-05, 0.00013324010268117494, 0.36053070352531963],
+                                     [0.00015367373294378274, 4.031149574591397e-06, 0.09989792406106686],
+                                     [0.00011108885799982233, 0.00015417631861587049, 0.17159666587495342],
                                      ]
+
+# # False alarms check
+# for k in range(len(stdtrans_normtoab_probthred_combs)):
+#     df_k = copy.deepcopy(df_raw)
+
+#     data_processor_k = DataProcess(
+#         data=df_k,
+#         time_covariates=["week_of_year"],
+#         train_split=0.3,
+#         validation_split=0.1,
+#         output_col=output_col,
+#     )
+#     _, _, test_data_k, normalized_data = data_processor_k.get_splits()
+
+#     # Define SKF model
+#     norm_model = Model(
+#         LocalTrend(mu_states=model_dict["mu_states"][0:2].reshape(-1), var_states=[1e-12, 1e-12]),
+#         LSTM,
+#         Autoregression(std_error=np.sqrt(model_dict['states_optimal'].mu_prior[-1][W2bar_index].item()), 
+#                    phi=model_dict['states_optimal'].mu_prior[-1][phi_index].item(), 
+#                    mu_states=[model_dict["mu_states"][autoregression_index].item()], 
+#                    var_states=[model_dict["var_states"][autoregression_index, autoregression_index].item()]),
+#     )
+#     norm_model.lstm_net.load_state_dict(model_dict["lstm_network_params"])
+
+#     abnorm_model = Model(
+#         LocalAcceleration(mu_states=[model_dict["mu_states"][0].item(), model_dict["mu_states"][1].item(), 0], var_states=[1e-12, 1e-12, 1e-4]),
+#         LSTM,
+#         Autoregression(std_error=np.sqrt(model_dict['states_optimal'].mu_prior[-1][W2bar_index].item()), 
+#                    phi=model_dict['states_optimal'].mu_prior[-1][phi_index].item(), 
+#                    mu_states=[model_dict["mu_states"][autoregression_index].item()], 
+#                    var_states=[model_dict["var_states"][autoregression_index, autoregression_index].item()]),
+#     )
+#     abnorm_model.lstm_net.load_state_dict(model_dict["lstm_network_params"])
+#     skf = SKF(
+#             norm_model=norm_model,
+#             abnorm_model=abnorm_model,
+#             std_transition_error=0,     # To be replaced
+#             norm_to_abnorm_prob=0,      # To be replaced
+#             abnorm_to_norm_prob=1e-1,
+#             norm_model_prior_prob=0.99,
+#         )
+#     skf.std_transition_error = stdtrans_normtoab_probthred_combs[k][0]
+#     skf.norm_to_abnorm_prob = stdtrans_normtoab_probthred_combs[k][1]
+#     prob_anm_threshold = stdtrans_normtoab_probthred_combs[k][2]
+
+#     skf._initialize_attributes()
+#     skf._initialize_model(norm_model, abnorm_model)
+
+#     skf.filter_marginal_prob_history = skf._prob_history()
+#     skf._set_same_states_transition_models()
+#     skf.initialize_states_history()
+
+#     filter_marginal_abnorm_prob, states = skf.filter(data=normalized_data)
+
+#     p_anm_all = filter_marginal_abnorm_prob
+
+#     if (np.array(p_anm_all) > prob_anm_threshold).any():
+#         print(f"False alarm detected for combination {k}: {stdtrans_normtoab_probthred_combs[k]}")
+# 1/0
 
 norm_const_std = data_processor.scale_const_std[data_processor.output_col]
 
@@ -296,6 +364,6 @@ for k in tqdm(range(len(restored_data))):
 
     # plt.show()
 
-# Save the results to a CSV file
-results_df = pd.DataFrame(results_all, columns=["anomaly_magnitude", "anomaly_start_index", "anomaly_detected_index", "mse_LL", "mse_LT", "detection_time"])
-results_df.to_csv("saved_results/prob_eva/detrended_ts5_results_skf_better_ssm.csv", index=False)
+# # Save the results to a CSV file
+# results_df = pd.DataFrame(results_all, columns=["anomaly_magnitude", "anomaly_start_index", "anomaly_detected_index", "mse_LL", "mse_LT", "detection_time"])
+# results_df.to_csv("saved_results/prob_eva/detrended_ts5_results_skf_better_ssm.csv", index=False)
