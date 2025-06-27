c105f46bafbf68fcef32cea74bce9b72585e880e
zhanwenxin22 <zhanwen.w.xin@gmail.com>
2025-06-27 19:46:17 -0400

    prophet for detrend ts5

--- examples/real_ts5_prophet_online.py
+++ examples/real_ts5_prophet_online.py
@@ -7,41 +7,47 @@ import numpy as np
 import os
 os.environ['OMP_NUM_THREADS'] = '1'
 
-# Read another csv file and store it in df["ds"]
-data_file_time = "./data/toy_time_series/synthetic_simple_autoregression_periodic_datetime.csv"
-df_raw = pd.read_csv(data_file_time, skiprows=1, delimiter=",", header=None)
-df_raw[0] = pd.to_datetime(df_raw[0]).dt.strftime('%Y-%m-%d')
-df_raw.columns = ["ds"]
+# # Read another csv file and store it in df["ds"]
+# data_file_time = "./data/toy_time_series/synthetic_simple_autoregression_periodic_datetime.csv"
+# df_raw = pd.read_csv(data_file_time, skiprows=1, delimiter=",", header=None)
+# df_raw[0] = pd.to_datetime(df_raw[0]).dt.strftime('%Y-%m-%d')
+# df_raw.columns = ["ds"]
 
-data_file = "./data/toy_time_series/synthetic_simple_autoregression_periodic.csv"
-values = pd.read_csv(data_file, skiprows=1, delimiter=",", header=None)
-df_raw["y"] = values
-df_raw.head()
+# data_file = "./data/toy_time_series/synthetic_simple_autoregression_periodic.csv"
+# values = pd.read_csv(data_file, skiprows=1, delimiter=",", header=None)
+# df_raw["y"] = values
+# df_raw.head()
 
-# LT anomaly
-# anm_mag = 0.010416667/10
-anm_start_index = 52*10
-anm_mag = 0.3/52
-# anm_baseline = np.linspace(0, 3, num=len(df_raw))
-anm_baseline = np.arange(len(df_raw)) * anm_mag
-# Set the first 52*12 values in anm_baseline to be 0
-anm_baseline[anm_start_index:] -= anm_baseline[anm_start_index]
-anm_baseline[:anm_start_index] = 0
+data_file = "./data/benchmark_data/detrended_data/test_5_data_detrended.csv"
+df_raw = pd.read_csv(data_file, skiprows=1, delimiter=",", header=None)
+time_series = pd.to_datetime(df_raw.iloc[:, 0])
+# Set the first column name to "ds"
+df_raw.columns = ['ds', 'y']
+
+print(df_raw)
+
+
+# # LT anomaly
+# # anm_mag = 0.010416667/10
+# anm_start_index = 52*10
+# anm_mag = 0.3/52
+# # anm_baseline = np.linspace(0, 3, num=len(df_raw))
+# anm_baseline = np.arange(len(df_raw)) * anm_mag
+# # Set the first 52*12 values in anm_baseline to be 0
+# anm_baseline[anm_start_index:] -= anm_baseline[anm_start_index]
+# anm_baseline[:anm_start_index] = 0
 
 # # LL anomaly
 # anm_mag = 0.5
 # anm_baseline = np.zeros_like(df_raw)
 # anm_baseline[anm_start_index:] += anm_mag
 
-df_raw["y"] = df_raw["y"].add(anm_baseline, axis=0)
-# Remove the last 52*5 rows in df_raw
-df_raw = df_raw[:-52*5]
 
 df_raw = df_raw.iloc[:int(len(df_raw) * 1)]
 
 # Genetrate percentages_check from 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, ... , 1
 # percentages_check = [i / 100 for i in range(10, 101, 1)]
-begin_idx = int(len(df_raw) * 0.1)
+begin_idx = int(len(df_raw) * 0.4)
 
 # changepoint_prior_scale = 0.01
 threshold = 0.5
@@ -72,8 +78,8 @@ for i in range(len(df_raw)-begin_idx):
     # print(forecast)
     fig1 = m.plot(forecast)
     a = add_changepoints_to_plot(fig1.gca(), m, forecast, threshold=threshold)
-    fig1.gca().set_ylim(-1.508410797906945, 2.650273557375443)
-    fig1.gca().set_xlim(10684.35, 16682.65)
+    # fig1.gca().set_ylim(-1.508410797906945, 2.650273557375443)
+    fig1.gca().set_xlim(14384.85, 20152.15)
 
     # Plot the anomalies in figure 1
     # for i in range(len(forecast)):
@@ -116,7 +122,10 @@ m = Prophet(changepoint_range=1, changepoints=signif_changepoints)
 m.fit(df_raw)
 forecast = m.predict(df_raw)
 fig1 = m.plot(forecast)
-plt.axvline(x=m.history['ds'][anm_start_index], color='k', linestyle='--')
+a = add_changepoints_to_plot(fig1.gca(), m, forecast, threshold=threshold)
+print(fig1.gca().get_xlim())
+print(fig1.gca().get_ylim())
+# plt.axvline(x=m.history['ds'][anm_start_index], color='k', linestyle='--')
 if anm_detect_point is not None:
     plt.axvline(x=m.history['ds'][anm_detect_point], color='r', linestyle='--')
     for cp in signif_changepoints:
