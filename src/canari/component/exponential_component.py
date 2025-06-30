ec3b63515c6caf229ddd3749f542ca431045bf55
MichelWU99 <michelwu29@gmail.com>
2025-06-30 15:08:39 -0400

    Ajout commentaires, version plus clean, test sur des données réelles, ajout de données synthétiques pour le toy exponential

--- src/canari/component/exponential_component.py
+++ src/canari/component/exponential_component.py
@@ -8,6 +8,41 @@ from canari.component.base_component import BaseComponent
 
 
 class Exponential(BaseComponent):
+    """
+    `Exponential` class, inheriting from Canari's `BaseComponent`.
+    It models exponential growth with a locally constant speed over time (linear level), which simulates the abscissa scale,
+    and a constant amplitude, which simulates the ordinate scale.
+
+    Args:
+        std_error (Optional[float]): Standard deviation of the process noise. Defaults: initialized to 0.
+        mu_states (Optional[list[float]]): Initial mean of the hidden state. Defaults:
+            initialized to zeros.
+        var_states (Optional[list[float]]): Initial variance of the hidden state. Defaults:
+            initialized to zeros.
+
+    Examples:
+        >>> from canari.component import Exponential
+        >>> # With known mu_states and var_states
+        >>> exponential = Exponential(mu_states=[0, 0.15, 10, 0, 0], var_states=[0.04, 0.01, 1, 0, 0], std_error=0.3)
+        >>> # With default mu_states and var_states
+        >>> exponential = Exponential(std_error=0.2)
+        >>> exponential.component_name
+        'exp'
+        >>> exponential.states_name
+        ['latent level', 'latent trend', 'scale', 'exp', 'scaled exp']
+        >>> exponential.transition_matrix
+        array([ [1, 1, 0, 0, 0],
+                [0, 1, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1],)
+        >>> exponential.observation_matrix
+        array([[0, 0, 0, 0, 1]])
+        >>> exponential.process_noise_matrix
+        >>> exponential.mu_states
+        >>> exponential.var_states
+    """
+
     def __init__(
         self,
         std_error: Optional[float] = 0.0,
@@ -27,11 +62,11 @@ class Exponential(BaseComponent):
 
     def initialize_states_name(self):
         self._states_name = [
-            "exp level",
-            "exp trend",
-            "exp amplitude",
+            "latent level",  # latent level
+            "latent trend",  # latent trend
+            "scale",  # scale
             "exp",
-            "exp with amplitude",
+            "scaled exp",  # scaled exp
         ]
 
     def initialize_transition_matrix(self):
@@ -64,6 +99,7 @@ class Exponential(BaseComponent):
             self._mu_states = np.zeros((self._num_states, 1))
         elif len(self._mu_states) == self._num_states:
             self._mu_states = np.atleast_2d(self._mu_states).T
+            self._mu_states[0] = -self._mu_states[1]
         else:
             raise ValueError(
                 "Incorrect mu_states dimension for the exponential component."
