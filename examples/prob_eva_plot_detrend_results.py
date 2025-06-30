a0877648c53d9bbdcab05b5ddb10c0f6c314a12b
zhanwenxin22 <zhanwen.w.xin@gmail.com>
2025-06-30 10:15:22 -0400

    Update prob_eva_plot_detrend_results.py

--- examples/prob_eva_plot_detrend_results.py
+++ examples/prob_eva_plot_detrend_results.py
@@ -20,13 +20,13 @@ df_il = pd.read_csv("saved_results/prob_eva/detrended_ts5better_results_il.csv")
 df_skf = pd.read_csv("saved_results/prob_eva/detrended_ts5_results_skf_better_ssm.csv")
 df_skf_whitenoise = pd.read_csv("saved_results/prob_eva/detrended_ts5_whitenoise_skf.csv")
 
-# df_prophet = pd.read_csv("saved_results/prob_eva/toy_simple_results_prophet_online_baseline.csv")
+df_prophet = pd.read_csv("saved_results/prob_eva/detrended_ts5_results_prophet_online.csv")
 
 # Multiply the df_il["anomaly_magnitude"] by 52
 df_il["anomaly_magnitude"] = np.abs(df_il["anomaly_magnitude"]) * 52
 df_skf["anomaly_magnitude"] = np.abs(df_skf["anomaly_magnitude"]) * 52
 df_skf_whitenoise["anomaly_magnitude"] = np.abs(df_skf_whitenoise["anomaly_magnitude"]) * 52
-# df_prophet["anomaly_magnitude"] = df_prophet["anomaly_magnitude"] * 52
+df_prophet["anomaly_magnitude"] = df_prophet["anomaly_magnitude"] * 52
 
 # Compute detection_rate, for each anomaly magnitude, when df_il["detection_time"] == 260, it means that the anomaly is not detected
 df_il["detection_rate"] = df_il["detection_time"].apply(
@@ -38,9 +38,9 @@ df_skf["detection_rate"] = df_skf["detection_time"].apply(
 df_skf_whitenoise["detection_rate"] = df_skf_whitenoise["detection_time"].apply(
     lambda x: 0 if x >= 52 * 3 else 1
 )
-# df_prophet["detection_rate"] = df_prophet["detection_time"].apply(
-#     lambda x: 0 if x == 260 else 1
-# )
+df_prophet["detection_rate"] = df_prophet["detection_time"].apply(
+    lambda x: 0 if x >= 52 * 3 else 1
+)
 
 # Get anomaly_detected_index from df_prophet["anomaly_detected_index"]
 df_il["anomaly_detected_index"] = df_il["anomaly_detected_index"].apply(ast.literal_eval)
@@ -55,10 +55,10 @@ df_skf_whitenoise["anomaly_detected_index"] = df_skf_whitenoise["anomaly_detecte
 df_skf_whitenoise["false_alarms_num"] = df_skf_whitenoise["anomaly_detected_index"].apply(
     lambda x: len(x) - 1 if len(x) > 1 else 0
 )
-# df_prophet["anomaly_detected_index"] = df_prophet["anomaly_detected_index"].apply(ast.literal_eval)
-# df_prophet["false_alarms_num"] = df_prophet["anomaly_detected_index"].apply(
-#     lambda x: len(x) - 1 if len(x) > 1 else 0
-# )
+df_prophet["anomaly_detected_index"] = df_prophet["anomaly_detected_index"].apply(ast.literal_eval)
+df_prophet["false_alarms_num"] = df_prophet["anomaly_detected_index"].apply(
+    lambda x: len(x) - 1 if len(x) > 1 else 0
+)
 
 # For the same anomaly magnitude, compute the mean and variance of df_il["mse_LL"], df_il["mse_LT"], and df_il["detection_time"], stored them in a new dataframe
 df_il_mean = df_il.groupby("anomaly_magnitude").agg(
@@ -89,15 +89,15 @@ df_skf_whitenoise_mean = df_skf_whitenoise.groupby("anomaly_magnitude").agg(
     }
 )
 
-# df_prophet_mean = df_prophet.groupby("anomaly_magnitude").agg(
-#     {
-#         "mse_LL": ["mean", "std"],
-#         "mse_LT": ["mean", "std"],
-#         "detection_time": ["mean", "std"],
-#         "detection_rate": ["mean", "std"],
-#         "false_alarms_num": ["mean", "std"],
-#     }
-# )
+df_prophet_mean = df_prophet.groupby("anomaly_magnitude").agg(
+    {
+        "mse_LL": ["mean", "std"],
+        "mse_LT": ["mean", "std"],
+        "detection_time": ["mean", "std"],
+        "detection_rate": ["mean", "std"],
+        "false_alarms_num": ["mean", "std"],
+    }
+)
 
 # Plot the mean and std of df_il["mse_LL"], df_il["mse_LT"], and df_il["detection_time"] for each anomaly magnitude
 fig, ax = plt.subplots(5, 1, figsize=(10, 6))
@@ -125,13 +125,13 @@ ax[0].fill_between(
     df_skf_whitenoise_mean["mse_LL"]["mean"] + df_skf_whitenoise_mean["mse_LL"]["std"],
     alpha=0.2,
 )
-# ax[0].plot(df_prophet_mean.index, df_prophet_mean["mse_LL"]["mean"], label="Prophet")
-# ax[0].fill_between(
-#     df_prophet_mean.index,
-#     df_prophet_mean["mse_LL"]["mean"] - df_prophet_mean["mse_LL"]["std"],
-#     df_prophet_mean["mse_LL"]["mean"] + df_prophet_mean["mse_LL"]["std"],
-#     alpha=0.2,
-# )
+ax[0].plot(df_prophet_mean.index, df_prophet_mean["mse_LL"]["mean"], label="Prophet")
+ax[0].fill_between(
+    df_prophet_mean.index,
+    df_prophet_mean["mse_LL"]["mean"] - df_prophet_mean["mse_LL"]["std"],
+    df_prophet_mean["mse_LL"]["mean"] + df_prophet_mean["mse_LL"]["std"],
+    alpha=0.2,
+)
 ax[0].set_ylabel(r"MSE($x^{\mathrm{LL}}$)")
 # ax[0].set_ylabel(r"MAPE($x^{\mathrm{LL}}$)")
 ax[0].legend()
@@ -161,13 +161,13 @@ ax[1].fill_between(
     df_skf_whitenoise_mean["mse_LT"]["mean"] + df_skf_whitenoise_mean["mse_LT"]["std"],
     alpha=0.2,
 )
-# ax[1].plot(df_prophet_mean.index, df_prophet_mean["mse_LT"]["mean"], label="Prophet")
-# ax[1].fill_between(
-#     df_prophet_mean.index,
-#     df_prophet_mean["mse_LT"]["mean"] - df_prophet_mean["mse_LT"]["std"],
-#     df_prophet_mean["mse_LT"]["mean"] + df_prophet_mean["mse_LT"]["std"],
-#     alpha=0.2,
-# )
+ax[1].plot(df_prophet_mean.index, df_prophet_mean["mse_LT"]["mean"], label="Prophet")
+ax[1].fill_between(
+    df_prophet_mean.index,
+    df_prophet_mean["mse_LT"]["mean"] - df_prophet_mean["mse_LT"]["std"],
+    df_prophet_mean["mse_LT"]["mean"] + df_prophet_mean["mse_LT"]["std"],
+    alpha=0.2,
+)
 ax[1].set_ylabel(r"MSE($x^{\mathrm{LT}}$)")
 # ax[1].set_ylabel(r"MAPE($x^{\mathrm{LT}}$)")
 # Format x-axis ticks with scientific notation
@@ -199,13 +199,13 @@ ax[2].fill_between(
     df_skf_whitenoise_mean["detection_time"]["mean"] + df_skf_whitenoise_mean["detection_time"]["std"],
     alpha=0.2,
 )
-# ax[2].plot(df_prophet_mean.index, df_prophet_mean["detection_time"]["mean"], label="Prophet")
-# ax[2].fill_between(
-#     df_prophet_mean.index,
-#     df_prophet_mean["detection_time"]["mean"] - df_prophet_mean["detection_time"]["std"],
-#     df_prophet_mean["detection_time"]["mean"] + df_prophet_mean["detection_time"]["std"],
-#     alpha=0.2,
-# )
+ax[2].plot(df_prophet_mean.index, df_prophet_mean["detection_time"]["mean"], label="Prophet")
+ax[2].fill_between(
+    df_prophet_mean.index,
+    df_prophet_mean["detection_time"]["mean"] - df_prophet_mean["detection_time"]["std"],
+    df_prophet_mean["detection_time"]["mean"] + df_prophet_mean["detection_time"]["std"],
+    alpha=0.2,
+)
 ax[2].set_ylabel(r"$\Delta t\ (\mathrm{yr})$")
 # ax[2].set_yticks([0, 52, 104, 156, 208, 260])
 ax[2].set_yticks([0, 52, 104, 156])
@@ -218,7 +218,7 @@ ax[2].set_xticklabels([])
 ax[3].plot(df_il_mean.index, df_il_mean["detection_rate"]["mean"], label="IL")
 ax[3].plot(df_skf_mean.index, df_skf_mean["detection_rate"]["mean"], label="SKF")
 ax[3].plot(df_skf_whitenoise_mean.index, df_skf_whitenoise_mean["detection_rate"]["mean"], label="SKF (whitenoise)")
-# ax[3].plot(df_prophet_mean.index, df_prophet_mean["detection_rate"]["mean"], label="Prophet")
+ax[3].plot(df_prophet_mean.index, df_prophet_mean["detection_rate"]["mean"], label="Prophet")
 # ax[3].set_xlabel("Anomaly Magnitude (unit/year)")
 ax[3].set_ylabel(r"$\mathcal{P}_{\mathrm{detect}}$")
 # ax[3].set_ylabel(r"$\Pr_{\mathrm{detect}}$")
@@ -254,14 +254,14 @@ ax[4].fill_between(
     alpha=0.2,
     color = "tab:green"
 )
-# ax[4].plot(df_prophet_mean.index, df_prophet_mean["false_alarms_num"]["mean"], label="Prophet", color = "tab:green")
-# ax[4].fill_between(
-#     df_prophet_mean.index,
-#     df_prophet_mean["false_alarms_num"]["mean"] - df_prophet_mean["false_alarms_num"]["std"],
-#     df_prophet_mean["false_alarms_num"]["mean"] + df_prophet_mean["false_alarms_num"]["std"],
-#     alpha=0.2,
-#     color = "tab:green"
-# )
+ax[4].plot(df_prophet_mean.index, df_prophet_mean["false_alarms_num"]["mean"], label="Prophet", color = "tab:green")
+ax[4].fill_between(
+    df_prophet_mean.index,
+    df_prophet_mean["false_alarms_num"]["mean"] - df_prophet_mean["false_alarms_num"]["std"],
+    df_prophet_mean["false_alarms_num"]["mean"] + df_prophet_mean["false_alarms_num"]["std"],
+    alpha=0.2,
+    color = "tab:red"
+)
 ax[4].set_xlabel("Anomaly Magnitude (unit/year)")
 ax[4].set_ylabel("\# re. alarms")
 ax[4].set_xscale('log')
