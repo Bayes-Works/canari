b0f7e528b9a3382915b208cd04a61757faf31633
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2025-08-01 16:41:51 -0400

    save model preds history and early stop

--- examples/toy_forecast_dep.py
+++ examples/toy_forecast_dep.py
@@ -15,20 +15,25 @@ from canari import (
 from canari.component import LocalTrend, LstmNetwork, WhiteNoise
 
 # # Read data
-data_file = "./data/benchmark_data/test_5_data.csv"
+data_file = "./data/toy_time_series/toy_time_series_dependency_1.csv"
 df_raw = pd.read_csv(data_file, skiprows=1, delimiter=",", header=None)
-time_series = pd.to_datetime(df_raw.iloc[:, 0])
-df_raw = df_raw.iloc[:, 1:]
+df_raw.columns = ["exp_sine", "sine"]
+lags = [0, 9]
+df_raw = DataProcess.add_lagged_columns(df_raw, lags)
+
+data_file_time = "./data/toy_time_series/sine_datetime.csv"
+time_series = pd.read_csv(data_file_time, skiprows=1, delimiter=",", header=None)
+time_series = pd.to_datetime(time_series[0])
 df_raw.index = time_series
 df_raw.index.name = "date_time"
-df_raw.columns = ["y", "water_level", "temp_min", "temp_max"]
+
 # Data pre-processing
 output_col = [0]
 data_processor = DataProcess(
     data=df_raw,
-    time_covariates=["week_of_year"],
-    train_split=0.289,
-    validation_split=0.0693,
+    time_covariates=["hour_of_day"],
+    train_split=0.8,
+    validation_split=0.2,
     output_col=output_col,
 )
 train_data, validation_data, test_data, all_data = data_processor.get_splits()
@@ -37,41 +42,48 @@ train_data, validation_data, test_data, all_data = data_processor.get_splits()
 model_target = Model(
     LocalTrend(),
     LstmNetwork(
-        look_back_len=17,
-        num_features=5,
+        look_back_len=1,
+        num_features=12,
         num_layer=1,
         num_hidden_unit=50,
         device="cpu",
         manual_seed=1,
+        model_noise=True,
     ),
-    WhiteNoise(std_error=0.028761848586134717),
+    # WhiteNoise(std_error=1e-1),
 )
+
 model_target.model_type = "target"
 model_target.auto_initialize_baseline_states(train_data["y"][0:24])
 
 # Dependent model
 model_covar = Model(
     LstmNetwork(
-        look_back_len=19,
+        look_back_len=10,
         num_features=2,
         num_layer=1,
         num_hidden_unit=50,
         device="cpu",
         manual_seed=1,
+        model_noise=True,
     ),
-    WhiteNoise(std_error=1e-2),
+    # WhiteNoise(std_error=0.0032322250444898116),
 )
 model_covar.model_type = "covariate"
-model_covar.output_col = [0]
-model_covar.input_col = [3]
+model_covar.output_col = [1]
+# model_covar.input_col = [2]
+model_covar.output_lag_col = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+model_covar.input_col = [11]
+
 # Ensemble Model
 model = ModelEnsemble(model_target, model_covar)
-
+model.recal_covariates_col(data_processor.covariates_col)
 
 # Training
 num_epoch = 50
 for epoch in range(num_epoch):
-    (mu_validation_preds, std_validation_preds, states) = model.lstm_train(
+
+    (mu_validation_preds, std_validation_preds) = model.lstm_train(
         train_data=train_data,
         validation_data=validation_data,
     )
@@ -83,27 +95,44 @@ for epoch in range(num_epoch):
         data_processor.scale_const_mean[output_col],
         data_processor.scale_const_std[output_col],
     )
-
+    std_validation_preds = normalizer.unstandardize_std(
+        std_validation_preds,
+        data_processor.scale_const_std[output_col],
+    )
     # Calculate the log-likelihood metric
-    validation_obs = data_processor.get_data("validation")
+    validation_obs = data_processor.get_data("validation").flatten()
     mse = metric.mse(mu_validation_preds, validation_obs)
 
-    # # Early-stopping
-    # model.early_stopping(
-    #     evaluate_metric=mse, current_epoch=epoch, max_epoch=num_epoch, skip_epoch=0
-    # )
-    # if epoch == model.optimal_epoch:
-    #     mu_validation_preds_optim = mu_validation_preds
-    #     std_validation_preds_optim = std_validation_preds
-    #     states_optim = copy.copy(
-    #         states
-    #     )  # If we want to plot the states, plot those from optimal epoch
+    # Covariate predictions
+    val_time = data_processor.get_time(split="validation")
+    _, val_index, _ = data_processor.get_split_indices()
+    mu_val_covar = np.array(model_covar.output_history.mu)[val_index]
+    var_val_covar = np.array(model_covar.output_history.var)[val_index]
+    validation_obs_covar = data_processor.get_data("validation", column=[1]).flatten()
+    mse_cov = metric.mse(mu_val_covar, validation_obs_covar)
 
-    # if model.stop_training:
-    #     break
+    # Early-stopping
+    model_target.early_stopping(
+        evaluate_metric=mse, current_epoch=epoch, max_epoch=num_epoch, skip_epoch=10
+    )
+    model_covar.early_stopping(
+        evaluate_metric=mse_cov, current_epoch=epoch, max_epoch=num_epoch, skip_epoch=10
+    )
 
-# print(f"Optimal epoch       : {model.optimal_epoch}")
-# print(f"Validation MSE      :{model.early_stop_metric: 0.4f}")
+    if epoch == model_target.optimal_epoch:
+        mu_validation_preds_optim = mu_validation_preds
+        std_validation_preds_optim = std_validation_preds
+        states_optim = copy.copy(
+            model_target.states
+        )  # If we want to plot the states, plot those from optimal epoch
+
+    if model_target.stop_training:
+        break
+
+print(f"Optimal epoch target model     : {model_target.optimal_epoch}")
+print(f"Validation metric      :{model_target.early_stop_metric: 0.4f}")
+print(f"Optimal epoch covariate model      : {model_covar.optimal_epoch}")
+print(f"Validation metric      :{model_covar.early_stop_metric: 0.4f}")
 
 #  Plot
 fig, ax = plt.subplots(figsize=(10, 6))
@@ -115,13 +144,9 @@ plot_data(
 )
 plot_prediction(
     data_processor=data_processor,
-    mean_validation_pred=mu_validation_preds,
-    std_validation_pred=std_validation_preds,
+    mean_validation_pred=mu_validation_preds_optim,
+    std_validation_pred=std_validation_preds_optim,
     validation_label=[r"$\mu$", f"$\pm\sigma$"],
 )
 plt.legend()
 plt.show()
-
-
-# plot_states(data_processor=data_processor, states=states_optim, states_type="posterior")
-# plt.show()
