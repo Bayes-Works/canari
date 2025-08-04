a2ae904fb6bf4473604a41bb2fff196f85f0b8c3
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2025-08-04 14:09:11 -0400

    recal_covar_col and lags

--- src/canari/model_ensemble.py
+++ src/canari/model_ensemble.py
@@ -3,21 +3,25 @@ Emsemble of model
 """
 
 import numpy as np
-from typing import Optional, List, Tuple, Dict
+from typing import Optional, List, Tuple, Dict, Union
 from canari.model import Model
-from canari.data_struct import StatesHistory, OutputHistory
 
 
-class ModelEnsemble:
+class ModelAssemble:
     """
     Ensemble of model for modelling dependencies.
     """
 
     def __init__(
         self,
-        *model: Model,
+        target_model: Model,
+        covariate_model: Union[Model, List[Model]],
     ):
-        self.model = list(model)
+        self.target_model = target_model
+        self.covariate_model = (
+            covariate_model if isinstance(covariate_model, list) else [covariate_model]
+        )
+        self._recal_covar_col = True
 
     def forward(
         self,
@@ -29,55 +33,49 @@ class ModelEnsemble:
 
         mu_pred = []
         var_pred = []
-        # Covariate model
         mu_input_covariates = input_covariates.copy()
         var_input_covariates = np.zeros_like(input_covariates)
-        for model in self.model:
-            if model.model_type == "covariate":
-                # get model input
-                x = mu_input_covariates[model.input_col]
-                mu_pred_covar, var_pred_covar, *_ = model.forward(x)
 
-                # save output history
-                var_pred_covar_wo_noise = var_pred_covar - model.sched_sigma_v**2
-                model.output_history.save_output_history(
-                    mu_pred_covar, var_pred_covar_wo_noise
+        # Covariate model
+        for model in self.covariate_model:
+            # get model input
+            x = mu_input_covariates[model.input_col]
+            mu_pred_covar, var_pred_covar, *_ = model.forward(x)
+
+            # save output history
+            var_pred_covar_wo_noise = var_pred_covar - model.sched_sigma_v**2
+            model.output_history.save_output_history(
+                mu_pred_covar, var_pred_covar_wo_noise
+            )
+
+            # Replace in target model's input
+            mu_input_covariates[model.output_col] = mu_pred_covar
+            var_input_covariates[model.output_col] = var_pred_covar_wo_noise
+
+            # Replace lags
+            if len(model.output_lag_col) > 0:
+                mu_output_lag = model.output_history.mu[-len(model.output_lag_col) :]
+                mu_output_lag = np.concatenate(
+                    (
+                        np.flip(mu_output_lag),
+                        np.zeros(len(model.output_lag_col) - len(mu_output_lag)),
+                    )
                 )
-
-                # Replace in target model's input
-                mu_input_covariates[model.output_col] = mu_pred_covar
-                var_input_covariates[model.output_col] = var_pred_covar_wo_noise
-
-                # Replace lags
-                if len(model.output_lag_col) > 0:
-                    mu_output_lag = model.output_history.mu[
-                        -len(model.output_lag_col) :
-                    ]
-                    mu_output_lag = np.concatenate(
-                        (
-                            np.flip(mu_output_lag),
-                            np.zeros(len(model.output_lag_col) - len(mu_output_lag)),
-                        )
+                var_output_lag = model.output_history.var[-len(model.output_lag_col) :]
+                var_output_lag = np.concatenate(
+                    (
+                        np.flip(var_output_lag),
+                        np.zeros(len(model.output_lag_col) - len(var_output_lag)),
                     )
-                    var_output_lag = model.output_history.var[
-                        -len(model.output_lag_col) :
-                    ]
-                    var_output_lag = np.concatenate(
-                        (
-                            np.flip(var_output_lag),
-                            np.zeros(len(model.output_lag_col) - len(var_output_lag)),
-                        )
-                    )
-                    mu_input_covariates[model.output_lag_col] = mu_output_lag
-                    var_input_covariates[model.output_lag_col] = var_output_lag
+                )
+                mu_input_covariates[model.output_lag_col] = mu_output_lag
+                var_input_covariates[model.output_lag_col] = var_output_lag
 
         # Target model
-        for model in self.model:
-            if model.model_type == "target":
-                mu_pred, var_pred, *_ = model.forward(
-                    mu_input_covariates, var_input_covariates
-                )
-                model.output_history.save_output_history(mu_pred, var_pred)
+        mu_pred, var_pred, *_ = self.target_model.forward(
+            mu_input_covariates, var_input_covariates
+        )
+        self.target_model.output_history.save_output_history(mu_pred, var_pred)
 
         return mu_pred, var_pred
 
@@ -88,33 +86,39 @@ class ModelEnsemble:
     ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """ """
 
-        for model in self.model:
-            if model.model_type == "covariate":
-                obs_temp = covariates[model.output_col].copy()
-            else:
-                obs_temp = obs.copy()
-
+        # Covariate models
+        for model in self.covariate_model:
+            obs_temp = covariates[model.output_col].copy()
             (
-                delta_mu_states,
-                delta_var_states,
+                delta_mu_states_covar,
+                delta_var_states_covar,
                 *_,
             ) = model.backward(obs_temp)
-
             if model.lstm_net:
-                model.update_lstm_param(delta_mu_states, delta_var_states)
+                model.update_lstm_param(delta_mu_states_covar, delta_var_states_covar)
+
+        # Target model
+        (
+            delta_mu_states,
+            delta_var_states,
+            *_,
+        ) = self.target_model.backward(obs)
+        if self.target_model.lstm_net:
+            self.target_model.update_lstm_param(delta_mu_states, delta_var_states)
 
     def forecast(self, data: Dict[str, np.ndarray]) -> Tuple[np.ndarray, np.ndarray]:
         """ """
 
         mu_obs_preds = []
         std_obs_preds = []
+
         for x in data["x"]:
             (
                 mu_obs_pred,
                 var_obs_pred,
             ) = self.forward(x)
 
-            for model in self.model:
+            for model in [self.target_model] + self.covariate_model:
                 if model.lstm_net:
                     model.update_lstm_history(
                         model.mu_states_prior, model.var_states_prior
@@ -143,7 +147,7 @@ class ModelEnsemble:
         mu_obs_preds = []
         std_obs_preds = []
 
-        for model in self.model:
+        for model in [self.target_model] + self.covariate_model:
             model.initialize_states_history()
             model.output_history.initialize()
 
@@ -155,7 +159,7 @@ class ModelEnsemble:
 
             self.backward(x, y)
 
-            for model in self.model:
+            for model in [self.target_model] + self.covariate_model:
                 if model.lstm_net:
                     model.update_lstm_history(
                         model.mu_states_posterior, model.var_states_posterior
@@ -178,7 +182,7 @@ class ModelEnsemble:
     ):
         """ """
 
-        for model in self.model:
+        for model in [self.target_model] + self.covariate_model:
             model.smoother(matrix_inversion_tol=matrix_inversion_tol, tol_type=tol_type)
 
     def lstm_train(
@@ -193,8 +197,12 @@ class ModelEnsemble:
         Train LSTM
         """
 
+        self._recal_covariates_col(
+            train_data["covariates_col"], train_data["data_col_names"]
+        )
+
         if white_noise_decay:
-            for model in self.model:
+            for model in [self.target_model] + self.covariate_model:
                 for noise_type in ("white noise", "heteroscedastic noise"):
                     if model.get_states_index(noise_type) is not None:
                         model.white_noise_decay(
@@ -203,6 +211,7 @@ class ModelEnsemble:
                             white_noise_decay_factor,
                         )
                         break
+
         self.filter(train_data)
         self.smoother()
         mu_validation_preds, std_validation_preds = self.forecast(validation_data)
@@ -215,17 +224,23 @@ class ModelEnsemble:
     def set_memory(self, time_step: int):
         """ """
         if time_step == 0:
-            for model in self.model:
+            for model in [self.target_model] + self.covariate_model:
                 model.set_memory(states=model.states, time_step=time_step)
 
-    def recal_covariates_col(self, covariates_col: np.ndarray):
+    def _recal_covariates_col(self, covariates_col: np.ndarray, data_col_names: list):
         """
         Re-estimate model.input_col, model.output_col, and model.output_lag_col
         following data["x"].
         """
 
-        for model in self.model:
-            if model.model_type == "covariate":
+        if self._recal_covar_col:
+            for model in self.covariate_model:
+                output_name = data_col_names[model.output_col[0]]
+                model.output_lag_col = [
+                    i
+                    for i, col in enumerate(data_col_names)
+                    if f"{output_name}_lag" in col
+                ]
                 covariates_index = np.flatnonzero(covariates_col)
                 if len(model.input_col) > 0:
                     model.input_col = [
@@ -239,3 +254,6 @@ class ModelEnsemble:
                         np.where(covariates_index == i)[0][0]
                         for i in model.output_lag_col
                     ]
+
+            # set to False, do recal_covariates_col() only once
+            self._recal_covar_col = False
