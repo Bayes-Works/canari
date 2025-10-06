77c78e7c3f29d91003c7d4490df9b535d1017f9d
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2025-10-06 10:20:02 -0400

    update benchmarks

--- examples/benchmark/BM1.py
+++ examples/benchmark/BM1.py
@@ -21,11 +21,12 @@ from canari.component import LocalTrend, LocalAcceleration, LstmNetwork, WhiteNo
 
 
 def main(
-    num_trial_optim_model: int = 100,
-    num_trial_optim_skf: int = 200,
-    param_optimization: bool = True,
+    num_trial_optim_model: int = 50,
+    num_trial_optim_skf: int = 300,
+    param_optimization: bool = False,
     param_grid_search: bool = False,
-    smoother: bool = False,
+    smoother: bool = True,
+    plot: bool = False,
 ):
     ######### Data processing #########
     # Read data
@@ -54,7 +55,7 @@ def main(
         model = Model(
             LocalTrend(),
             LstmNetwork(
-                look_back_len=param["look_back_len"],
+                look_back_len=int(param["look_back_len"]),
                 num_features=2,
                 num_layer=1,
                 infer_len=52 * 3,
@@ -163,7 +164,7 @@ def main(
         if param_optimization:
             param_space = {
                 "look_back_len": [12, 52],
-                "sigma_v": [3e-1, 4e-1],
+                "sigma_v": [1e-1, 4e-1],
             }
         elif param_grid_search:
             param_space = {
@@ -189,30 +190,31 @@ def main(
         )
 
         # Plot
-        fig, ax = plt.subplots(figsize=(10, 6))
-        plot_data(
-            data_processor=data_processor,
-            standardization=True,
-            plot_test_data=False,
-            plot_column=output_col,
-            validation_label="y",
-        )
-        plot_prediction(
-            data_processor=data_processor,
-            mean_validation_pred=mu_validation_preds,
-            std_validation_pred=std_validation_preds,
-            validation_label=["mean", "std"],
-        )
-        plot_states(
-            data_processor=data_processor,
-            states=model_optim.states,
-            standardization=True,
-            states_to_plot=["level"],
-            sub_plot=ax,
-        )
-        plt.legend()
-        plt.title("Validation predictions")
-        plt.show()
+        if plot:
+            fig, ax = plt.subplots(figsize=(10, 6))
+            plot_data(
+                data_processor=data_processor,
+                standardization=True,
+                plot_test_data=False,
+                plot_column=output_col,
+                validation_label="y",
+            )
+            plot_prediction(
+                data_processor=data_processor,
+                mean_validation_pred=mu_validation_preds,
+                std_validation_pred=std_validation_preds,
+                validation_label=["mean", "std"],
+            )
+            plot_states(
+                data_processor=data_processor,
+                states=model_optim.states,
+                standardization=True,
+                states_to_plot=["level"],
+                sub_plot=ax,
+            )
+            plt.legend()
+            plt.title("Validation predictions")
+            plt.show()
         # Save best model for SKF analysis later
         model_optim_dict = model_optim.get_dict(time_step=0)
 
@@ -220,34 +222,35 @@ def main(
         # Define parameter search space
         slope_upper_bound = 5e-2
         slope_lower_bound = 1e-3
-        # # Plot synthetic anomaly
-        synthetic_anomaly_data = DataProcess.add_synthetic_anomaly(
-            train_data,
-            num_samples=1,
-            slope=[slope_lower_bound, slope_upper_bound],
-        )
-        plot_data(
-            data_processor=data_processor,
-            standardization=True,
-            plot_validation_data=False,
-            plot_test_data=False,
-            plot_column=output_col,
-            train_label="data without anomaly",
-        )
+        if plot:
+            # # Plot synthetic anomaly
+            synthetic_anomaly_data = DataProcess.add_synthetic_anomaly(
+                train_data,
+                num_samples=1,
+                slope=[slope_lower_bound, slope_upper_bound],
+            )
+            plot_data(
+                data_processor=data_processor,
+                standardization=True,
+                plot_validation_data=False,
+                plot_test_data=False,
+                plot_column=output_col,
+                train_label="data without anomaly",
+            )
 
-        train_time = data_processor.get_time("train")
-        for ts in synthetic_anomaly_data:
-            plt.plot(train_time, ts["y"])
-        plt.legend(
-            [
-                "data without anomaly",
-                "",
-                "smallest anomaly tested",
-                "largest anomaly tested",
-            ]
-        )
-        plt.title("Train data with added synthetic anomalies")
-        plt.show()
+            train_time = data_processor.get_time("train")
+            for ts in synthetic_anomaly_data:
+                plt.plot(train_time, ts["y"])
+            plt.legend(
+                [
+                    "data without anomaly",
+                    "",
+                    "smallest anomaly tested",
+                    "largest anomaly tested",
+                ]
+            )
+            plt.title("Train data with added synthetic anomalies")
+            plt.show()
 
         if param_grid_search:
             skf_param_space = {
@@ -299,6 +302,7 @@ def main(
         model_prob=filter_marginal_abnorm_prob,
     )
     fig.suptitle("SKF hidden states", fontsize=10, y=1)
+    plt.savefig("./saved_results/BM1.png")
     plt.show()
 
 
--- examples/benchmark/BM9.py
+++ examples/benchmark/BM9.py
@@ -22,9 +22,11 @@ from canari.component import LocalTrend, LocalAcceleration, LstmNetwork, WhiteNo
 
 def main(
     num_trial_optim_model: int = 50,
-    num_trial_optim_skf: int = 100,
-    param_optimization: bool = True,
+    num_trial_optim_skf: int = 300,
+    param_optimization: bool = False,
     param_grid_search: bool = False,
+    smoother: bool = True,
+    plot: bool = False,
 ):
     ######### Data processing #########
     # Read data
@@ -53,12 +55,13 @@ def main(
         model = Model(
             LocalTrend(),
             LstmNetwork(
-                look_back_len=param["look_back_len"],
+                look_back_len=int(param["look_back_len"]),
                 num_features=17,
                 num_layer=1,
                 infer_len=52 * 3,
                 num_hidden_unit=50,
                 manual_seed=1,
+                smoother=smoother,
             ),
             WhiteNoise(std_error=param["sigma_v"]),
         )
@@ -171,31 +174,33 @@ def main(
             param, train_data, validation_data
         )
 
-        # Plot
-        fig, ax = plt.subplots(figsize=(10, 6))
-        plot_data(
-            data_processor=data_processor,
-            standardization=True,
-            plot_test_data=False,
-            plot_column=output_col,
-            validation_label="y",
-        )
-        plot_prediction(
-            data_processor=data_processor,
-            mean_validation_pred=mu_validation_preds,
-            std_validation_pred=std_validation_preds,
-            validation_label=["mean", "std"],
-        )
-        plot_states(
-            data_processor=data_processor,
-            states=model_optim.states,
-            standardization=True,
-            states_to_plot=["level"],
-            sub_plot=ax,
-        )
-        plt.legend()
-        plt.title("Validation predictions")
-        plt.show()
+        if plot:
+            # Plot
+            fig, ax = plt.subplots(figsize=(10, 6))
+            plot_data(
+                data_processor=data_processor,
+                standardization=True,
+                plot_test_data=False,
+                plot_column=output_col,
+                validation_label="y",
+            )
+            plot_prediction(
+                data_processor=data_processor,
+                mean_validation_pred=mu_validation_preds,
+                std_validation_pred=std_validation_preds,
+                validation_label=["mean", "std"],
+            )
+            plot_states(
+                data_processor=data_processor,
+                states=model_optim.states,
+                standardization=True,
+                states_to_plot=["level"],
+                sub_plot=ax,
+            )
+            plt.legend()
+            plt.title("Validation predictions")
+            plt.show()
+
         # Save best model for SKF analysis later
         model_optim_dict = model_optim.get_dict(time_step=0)
 
@@ -203,34 +208,36 @@ def main(
         # Define parameter search space
         slope_upper_bound = 5e-2
         slope_lower_bound = 1e-3
-        # # Plot synthetic anomaly
-        synthetic_anomaly_data = DataProcess.add_synthetic_anomaly(
-            train_data,
-            num_samples=1,
-            slope=[slope_lower_bound, slope_upper_bound],
-        )
-        plot_data(
-            data_processor=data_processor,
-            standardization=True,
-            plot_validation_data=False,
-            plot_test_data=False,
-            plot_column=output_col,
-            train_label="data without anomaly",
-        )
 
-        train_time = data_processor.get_time("train")
-        for ts in synthetic_anomaly_data:
-            plt.plot(train_time, ts["y"])
-        plt.legend(
-            [
-                "data without anomaly",
-                "",
-                "smallest anomaly tested",
-                "largest anomaly tested",
-            ]
-        )
-        plt.title("Train data with added synthetic anomalies")
-        plt.show()
+        if plot:
+            # # Plot synthetic anomaly
+            synthetic_anomaly_data = DataProcess.add_synthetic_anomaly(
+                train_data,
+                num_samples=1,
+                slope=[slope_lower_bound, slope_upper_bound],
+            )
+            plot_data(
+                data_processor=data_processor,
+                standardization=True,
+                plot_validation_data=False,
+                plot_test_data=False,
+                plot_column=output_col,
+                train_label="data without anomaly",
+            )
+
+            train_time = data_processor.get_time("train")
+            for ts in synthetic_anomaly_data:
+                plt.plot(train_time, ts["y"])
+            plt.legend(
+                [
+                    "data without anomaly",
+                    "",
+                    "smallest anomaly tested",
+                    "largest anomaly tested",
+                ]
+            )
+            plt.title("Train data with added synthetic anomalies")
+            plt.show()
 
         if param_grid_search:
             skf_param_space = {
@@ -282,6 +289,7 @@ def main(
         model_prob=filter_marginal_abnorm_prob,
     )
     fig.suptitle("SKF hidden states", fontsize=10, y=1)
+    plt.savefig("./saved_results/BM9.png")
     plt.show()
 
 
