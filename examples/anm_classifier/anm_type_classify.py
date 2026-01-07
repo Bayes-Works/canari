8e33d456cbd671ce904d23f8e17c1a84bbea1ab0
zhanwenxin22 <zhanwen.w.xin@gmail.com>
2026-01-07 11:34:57 -0500

    initialization: 3 intervention models

--- examples/anm_classifier/anm_type_classify.py
+++ examples/anm_classifier/anm_type_classify.py
@@ -11,7 +11,7 @@ from canari import (
     plot_prediction,
     plot_states,
 )
-from src.hsl_classification_2classes_2itvmodels import hsl_classification
+from src.hsl_classification_2classes_3itvmodels import hsl_classification
 from src.matrix_profile_functions import past_only_matrix_profile
 import pytagi.metric as metric
 import pickle
