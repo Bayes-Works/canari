a65bb7d9b65d4b387a86c2d08c3bdeeee5455516
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2026-01-27 15:29:18 -0500

    Create forecast_m4.py

---
+++ examples/forecast_m4.py
@@ -0,0 +1,199 @@
+import copy
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pytagi.metric as metric
+from pytagi import Normalizer as normalizer
+from canari import DataProcess, Model, plot_data, plot_prediction, plot_states
+from canari.component import LstmNetwork, WhiteNoise, LocalTrend, ExpSmoothing, LocalLevel
+
+# # Read data
+ts = 59
+# training set
+data_train_file = "./data/m4/Hourly-train.csv"
+df_train = pd.read_csv(data_train_file, skiprows=1, delimiter=",", header=None)
+df_train = df_train.iloc[ts, 1:].to_frame()
+df_train = df_train.dropna()
+df_train = df_train.astype(float)
+# test set
+data_test_file = "./data/m4/Hourly-test.csv"
+df_test = pd.read_csv(data_test_file, skiprows=1, delimiter=",", header=None)
+df_test = df_test.iloc[ts, 1:].to_frame()
+df_test = df_test.dropna()
+df_test = df_test.astype(float)
+
+df = pd.concat([df_train, df_test], axis=0)
+
+train_start_time = pd.Timestamp(
+    year=2000,
+    month=1,
+    day=1,
+    hour=12,
+)
+
+df.index = pd.date_range(
+    start=train_start_time,
+    periods=len(df),
+    freq="H",
+)
+
+# Define parameters
+output_col = [0]
+num_epoch = 50
+nb_val = 12
+# Build data processor
+data_processor = DataProcess(
+    data=df,
+    train_start=df.index[0],
+    validation_start=df.index[len(df_train) - nb_val],
+    test_start=df.index[len(df_train)],
+    time_covariates=["hour_of_day"],
+    output_col=output_col,
+)
+# split data
+train_data, validation_data, test_data, _ = data_processor.get_splits()
+
+# Model
+model = Model(
+    LocalTrend(),
+    ExpSmoothing(),
+    LstmNetwork(
+        look_back_len=24,
+        num_features=2,
+        num_layer=1,
+        num_hidden_unit=50,
+        manual_seed=5,
+        model_noise=True,
+        model_exp_smooth=True,
+        smoother=False,
+    ),
+)
+
+model.auto_initialize_baseline_states(train_data["y"][0:24])
+
+# Training
+for epoch in range(num_epoch):
+    (mu_validation_preds, std_validation_preds, states) = model.lstm_train(
+        train_data=train_data,
+        validation_data=validation_data,
+    )
+
+    # Unstandardize the predictions
+    mu_validation_preds = normalizer.unstandardize(
+        mu_validation_preds,
+        data_processor.scale_const_mean[output_col],
+        data_processor.scale_const_std[output_col],
+    )
+    std_validation_preds = normalizer.unstandardize_std(
+        std_validation_preds,
+        data_processor.scale_const_std[output_col],
+    )
+
+
+    # Calculate the log-likelihood metric
+    # validation_obs = data_processor.get_data("validation").flatten()
+    # mse = metric.mse(mu_validation_preds, validation_obs)
+    # # Early-stopping
+    # model.early_stopping(evaluate_metric=mse, current_epoch=epoch, max_epoch=num_epoch)
+
+    validation_obs = data_processor.get_data("validation").flatten()
+    validation_log_lik = metric.log_likelihood(
+        prediction=mu_validation_preds,
+        observation=validation_obs,
+        std=std_validation_preds,
+    )
+
+    # Early-stopping
+    model.early_stopping(
+        evaluate_metric=-validation_log_lik, current_epoch=epoch, max_epoch=num_epoch
+    )
+
+
+    if epoch == model.optimal_epoch:
+        mu_validation_preds_optim = mu_validation_preds
+        std_validation_preds_optim = std_validation_preds
+        states_optim = copy.copy(
+            states
+        )  # If we want to plot the states, plot those from optimal epoch
+
+    if model.stop_training:
+        break
+
+
+print(f"Optimal epoch       : {model.optimal_epoch}")
+print(f"Validation MSE      :{model.early_stop_metric: 0.4f}")
+
+model.set_memory(
+    time_step=data_processor.test_start - 1,
+)
+
+# forecat on the test set
+mu_test_preds, std_test_preds, test_states = model.forecast(
+    data=test_data,
+)
+
+# Unstandardize the predictions
+mu_test_preds = normalizer.unstandardize(
+    mu_test_preds,
+    data_processor.scale_const_mean[output_col],
+    data_processor.scale_const_std[output_col],
+)
+std_test_preds = normalizer.unstandardize_std(
+    std_test_preds,
+    data_processor.scale_const_std[output_col],
+)
+
+# calculate the test metrics
+test_obs = data_processor.get_data("test").flatten()
+mse = metric.mse(mu_test_preds, test_obs)
+log_lik = metric.log_likelihood(mu_test_preds, test_obs, std_test_preds)
+
+print(f"Test MSE            :{mse: 0.4f}")
+print(f"Test Log-Lik        :{log_lik: 0.2f}")
+
+# plot the test data
+level_sum = states.get_mean(states_name="es") + states.get_mean(states_name="level") 
+
+# states.mu_posterior[idx_level,:] = level_sum
+
+for i in range(len(states.mu_posterior)):
+    states.mu_posterior[i][0] = level_sum[i]
+
+fig, ax = plot_states(
+    data_processor=data_processor,
+    states=states,
+    standardization=True,
+    color="b",
+)
+plot_data(
+    data_processor=data_processor,
+    standardization=True,
+    plot_column=output_col,
+    sub_plot=ax[0],
+)
+fig.suptitle("SKF hidden states", fontsize=10, y=1)
+plt.show()
+
+fig, ax = plt.subplots(figsize=(10, 6))
+plot_data(
+    data_processor=data_processor,
+    standardization=False,
+    plot_column=output_col,
+    validation_label="y",
+)
+plot_prediction(
+    data_processor=data_processor,
+    mean_validation_pred=mu_validation_preds_optim,
+    std_validation_pred=std_validation_preds_optim,
+    validation_label=[r"$\mu$", r"$\pm\sigma$"],
+)
+plot_prediction(
+    data_processor=data_processor,
+    mean_test_pred=mu_test_preds,
+    std_test_pred=std_test_preds,
+    test_label=[r"$\mu^{\prime}$", r"$\pm\sigma^{\prime}$"],
+    color="purple",
+)
+plt.legend(loc=(0.1, 1.01), ncol=6, fontsize=12)
+plt.tight_layout()
+plt.show()
