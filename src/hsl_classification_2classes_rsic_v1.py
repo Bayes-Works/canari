f47d7974b570f43a42c92f597c9cfd9e02599201
zhanwenxin22 <zhanwen.w.xin@gmail.com>
2026-02-27 15:31:01 -0500

    clean code 2

--- src/hsl_classification_2classes_rsic_v1.py
+++ src/hsl_classification_2classes_rsic_v1.py
@@ -64,7 +64,6 @@ class hsl_classification:
         self.y_std_scale = y_std_scale
         self._copy_initial_models()
         self.start_idx_mp = start_idx_mp
-        self.posterior_mu_states_no_itv, self.posterior_var_states_no_itv = [], []
 
     def _copy_initial_models(self):
         """
@@ -167,8 +166,6 @@ class hsl_classification:
             self.class_prob_moments.append([0.5, 0.5, 0, 0])
             self.ll_itv_all.append(0)
             self.lt_itv_all.append(0)
-            self.posterior_mu_states_no_itv.append(np.zeros_like(self.base_model.states.mu_posterior[0])*np.nan)
-            self.posterior_var_states_no_itv.append(np.zeros_like(self.base_model.states.mu_posterior[0])*np.nan)
 
             self.current_time_step += 1
 
@@ -838,10 +835,6 @@ class hsl_classification:
                 itv_applied_times.append(trigger_time)
                 print(f"LT intervention {itv_LT} is applied at time step {self.current_time_step}.")
 
-            if rerun_kf is False:
-                self.posterior_mu_states_no_itv.append(np.zeros_like(self.base_model.states.mu_posterior[0])*np.nan)
-                self.posterior_var_states_no_itv.append(np.zeros_like(self.base_model.states.mu_posterior[0])*np.nan)
-
             if apply_intervention:
                 if rerun_kf is False:
                     rerun_kf = True
@@ -868,17 +861,6 @@ class hsl_classification:
                     trigger = True
                     apply_intervention = False
                     first_time_trigger = False
-            # else:
-            #     if rerun_kf is False:
-            #         self.posterior_mu_states_no_itv.append(np.zeros_like(self.base_model.states.mu_posterior[0])*np.nan)
-            #     else:
-            #         self.posterior_var_states_no_itv.append(np.zeros_like(self.base_model.states.mu_posterior[0])*np.nan)
-
-            # if rerun_kf is False:
-            #     self.posterior_mu_states_no_itv.append(np.zeros_like(self.base_model.states.mu_posterior[0])*np.nan)
-            # else:
-            #     self.posterior_var_states_no_itv.append(np.zeros_like(self.base_model.states.mu_posterior[0])*np.nan)
-
 
             # Base model filter process
             mu_obs_pred, var_obs_pred, _, _ = self.base_model.forward(data["x"][i])
@@ -1755,10 +1737,6 @@ class hsl_classification:
     def _erase_history(self, num_steps_to_erase):
         # Erase the last num_steps_to_erase steps of the lstm history
         remove_until_index = -(num_steps_to_erase)
-
-        # Keep the states that are removed
-        self.posterior_mu_states_no_itv[remove_until_index:]  = self.base_model.states.mu_posterior[remove_until_index:]
-        self.posterior_var_states_no_itv[remove_until_index:] = self.base_model.states.var_posterior[remove_until_index:]
 
         self.base_model.states.mu_prior = self.base_model.states.mu_prior[:remove_until_index]
         self.base_model.states.var_prior = self.base_model.states.var_prior[:remove_until_index]
