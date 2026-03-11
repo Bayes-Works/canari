fix bug in data gen (anm2_mag missing sign) + prob eva wait 7 + skf develop

examples/anm_classifier/prob_eva/prob_eva_syn_simple_rsic_lltoll.py
@@ -213,8 +213,6 @@ for m in range(10):
         # True baselines
         true_LL_baseline = np.zeros(len(df_k))
         true_LT_baseline = np.zeros(len(df_k))
-        anm_LL_baseline = np.zeros(len(df_k))
-        anm_LT_baseline = np.zeros(len(df_k))
         # LL to LL anomaly
         true_LL_baseline[anm_start_index1:] = anm_mag1
         true_LL_baseline[anm_start_index2:] += np.ones(len(true_LL_baseline)-anm_start_index2) * anm_mag2
@@ -359,4 +357,4 @@ for m in range(10):
 
 # Save the results to a CSV file
 results_df = pd.DataFrame(results_all, columns=["anomaly_magnitude", "anomaly_start_index1", "anomaly_start_index2", "anomaly_detected_index", "intervention_log", "intervention_applied_times", "true_LL_baseline", "true_LT_baseline", "estimated_LL_baseline", "estimated_LT_baseline"])
-results_df.to_csv("saved_results/prob_eva/syn_simple_ts_results_rsic_v1_wait3_lltoll.csv", index=False)
+results_df.to_csv("saved_results/prob_eva/syn_simple_ts_results_rsic_v1_wait7_lltoll.csv", index=False)

examples/anm_classifier/prob_eva/prob_eva_syn_simple_rsic_lttoll.py
@@ -213,8 +213,6 @@ for m in range(10):
         # True baselines
         true_LL_baseline = np.zeros(len(df_k))
         true_LT_baseline = np.zeros(len(df_k))
-        anm_LL_baseline = np.zeros(len(df_k))
-        anm_LT_baseline = np.zeros(len(df_k))
         # LT to LL anomaly
         anm_mag1_perweek = anm_mag1 / 52
         true_LL_baseline[anm_start_index1:] += np.arange(len(true_LL_baseline)-anm_start_index1) * anm_mag1_perweek
@@ -361,4 +359,4 @@ for m in range(10):
 
 # Save the results to a CSV file
 results_df = pd.DataFrame(results_all, columns=["anomaly_magnitude", "anomaly_start_index1", "anomaly_start_index2", "anomaly_detected_index", "intervention_log", "intervention_applied_times", "true_LL_baseline", "true_LT_baseline", "estimated_LL_baseline", "estimated_LT_baseline"])
-results_df.to_csv("saved_results/prob_eva/syn_simple_ts_results_rsic_v1_wait3_lttoll.csv", index=False)
+results_df.to_csv("saved_results/prob_eva/syn_simple_ts_results_rsic_v1_wait7_lttoll.csv", index=False)

examples/anm_classifier/prob_eva/prob_eva_syn_simple_rsic_skf.py
@@ -40,6 +40,8 @@ data_processor = DataProcess(
     output_col=output_col,
 )
 
+scale_const_mean = copy.deepcopy(data_processor.scale_const_mean)
+scale_const_std = copy.deepcopy(data_processor.scale_const_std)
 train_data, validation_data, test_data, normalized_data = data_processor.get_splits()
 validation_start = df_raw.index[data_processor.validation_start]
 test_start = df_raw.index[data_processor.test_start]
@@ -238,6 +240,32 @@ for m in range(10):
         p_anm_all = filter_marginal_abnorm_prob
 
         all_detection_points = str(np.where(np.array(p_anm_all) > prob_anm_threshold)[0].tolist())
+        itv_log = []
+        itv_applied_times = []
+
+        # Get baselines for comparison
+        # Baseline estimation
+        mu_LL_states = states.get_mean(states_type='prior', states_name="level", standardization=True)
+        mu_LT_states = states.get_mean(states_type='prior', states_name="trend", standardization=True, scale_const_mean=scale_const_mean[0], scale_const_std=scale_const_std[0])
+        # True baselines
+        true_LL_baseline = np.zeros(len(df_k))
+        true_LT_baseline = np.zeros(len(df_k))
+        anm_LL_baseline = np.zeros(len(df_k))
+        anm_LT_baseline = np.zeros(len(df_k))
+        anm_mag2_perweek = anm_mag2 / 52
+        # LL to LT anomaly
+        true_LL_baseline[anm_start_index1:] = anm_mag1
+        true_LL_baseline[anm_start_index2:] += np.arange(len(true_LL_baseline)-anm_start_index2) * anm_mag2_perweek
+        true_LT_baseline[anm_start_index2:] = anm_mag2_perweek
+
+        # Convert the baselines to strings and save to results_all
+        true_LL_baseline_str = str(true_LL_baseline.tolist())
+        true_LT_baseline_str = str(true_LT_baseline.tolist())
+        estimate_LL_baseline_str = str(mu_LL_states.tolist())
+        estimate_LT_baseline_str = str(mu_LT_states.tolist())
+
+        results_all.append([anm_mag2, anm_start_index1, anm_start_index2, all_detection_points, itv_log, itv_applied_times, true_LL_baseline_str, true_LT_baseline_str, estimate_LL_baseline_str, estimate_LT_baseline_str])
+
 
         if (np.array(p_anm_all) > prob_anm_threshold).any():
             anm_detected_index = np.where(np.array(p_anm_all) > prob_anm_threshold)[0][0]
@@ -303,8 +331,12 @@ for m in range(10):
         # ax0.axvline(x=time[anm_start_index_global], color='r', linestyle='--')
         # ax0.set_xticklabels([])
         # ax0.set_title(f"SKF, mse_LL = {mse_LL:.3e}, mse_LT = {mse_LT:.3e}, detection_time = {detection_time}")
-        # ax0.plot(time, LL_baseline_true, color='k', linestyle='--')
-        # ax1.plot(time, LT_baseline_true, color='k', linestyle='--')
+        all_detection_points_list = np.where(np.array(p_anm_all) > prob_anm_threshold)[0].tolist()
+        for detection_point in all_detection_points_list:
+            ax0.axvline(x=time[int(detection_point)], color='r', linestyle='--')
+            ax4.axvline(x=time[int(detection_point)], color='r', linestyle='--')
+        ax0.plot(time, true_LL_baseline, color='k', linestyle='--')
+        ax1.plot(time, true_LT_baseline, color='k', linestyle='--')
         plot_states(
             data_processor=data_processor_k,
             standardization=True,

examples/anm_classifier/prob_eva/prob_syn_data_gen.py
@@ -66,10 +66,10 @@ model = Model(
 )
 
 num_test_ts = 10
-# LT anomaly magnitude
-anm_mag_all = np.concatenate([np.arange(0.01, 0.11, 0.01), np.arange(0.2, 1.01, 0.1)])
-# # LL anomaly magnitude
-# anm_mag_all = np.concatenate([np.arange(0.1, 2.01, 0.1)])
+# # LT anomaly magnitude
+# anm_mag_all = np.concatenate([np.arange(0.01, 0.11, 0.01), np.arange(0.2, 1.01, 0.1)])
+# LL anomaly magnitude
+anm_mag_all = np.concatenate([np.arange(0.1, 2.01, 0.1)])
 num_time_steps = 52 * 19
 gen_ts, _, _, _ = model.generate_time_series(num_time_series=num_test_ts*len(anm_mag_all),
                                             num_time_steps=num_time_steps)
@@ -96,6 +96,9 @@ for i, anm_mag in tqdm(enumerate(anm_mag_all)):
 
         sign = -1. if np.random.rand() < 0.5 else 1. # Randomly assign positive and negative anomalies
         anm1_mag_fixed *= sign
+        # # LL anomaly
+        # anm1_mag_unstandardize = anm1_mag_fixed * (scale_const_std[0] + 1e-10)  # Unstandardize the anomaly magnitude
+        # LT anomaly
         anm1_mag_perweek = anm1_mag_fixed / 52
         anm1_mag_unstandardize = anm1_mag_perweek * (scale_const_std[0] + 1e-10)  # Unstandardize the anomaly magnitude
 
@@ -108,31 +111,31 @@ for i, anm_mag in tqdm(enumerate(anm_mag_all)):
         # Second anomaly
         time_anomaly2 = time_anomaly1 + 52 * 6
 
-        # LT anomaly
-        anm2_mag = anm_mag/52
-        sign = -1. if np.random.rand() < 0.5 else 1. # Randomly assign positive and negative anomalies
-        anm2_mag *= sign
-        anm2_mag_unstandardize = anm2_mag * (scale_const_std[0] + 1e-10)  # Unstandardize the anomaly magnitude
-        anm2_baseline = np.arange(num_time_steps) * anm2_mag_unstandardize
-        anm2_baseline[time_anomaly2:] -= anm2_baseline[time_anomaly2]
-        anm2_baseline[:time_anomaly2] = 0
-        gen_anm_ts += anm2_baseline
-
-        # # LL anomaly
-        # anm2_mag = anm_mag
+        # # LT anomaly
+        # anm2_mag = anm_mag/52
         # sign = -1. if np.random.rand() < 0.5 else 1. # Randomly assign positive and negative anomalies
         # anm2_mag *= sign
-        # anm2_mag_unstandardize = anm2_mag * (scale_const_std[0] + 1e-10)
-        # anm2_baseline = np.ones(num_time_steps) * anm2_mag_unstandardize
+        # anm2_mag_unstandardize = anm2_mag * (scale_const_std[0] + 1e-10)  # Unstandardize the anomaly magnitude
+        # anm2_baseline = np.arange(num_time_steps) * anm2_mag_unstandardize
+        # anm2_baseline[time_anomaly2:] -= anm2_baseline[time_anomaly2]
         # anm2_baseline[:time_anomaly2] = 0
         # gen_anm_ts += anm2_baseline
 
+        # LL anomaly
+        anm2_mag = copy.deepcopy(anm_mag)
+        sign = -1. if np.random.rand() < 0.5 else 1. # Randomly assign positive and negative anomalies
+        anm2_mag *= sign
+        anm2_mag_unstandardize = anm2_mag * (scale_const_std[0] + 1e-10)
+        anm2_baseline = np.ones(num_time_steps) * anm2_mag_unstandardize
+        anm2_baseline[:time_anomaly2] = 0
+        gen_anm_ts += anm2_baseline
+
         values_str = str(list(gen_anm_ts))
-        time_series_all.append([values_str, anm1_mag_fixed, time_anomaly1, anm_mag, time_anomaly2])
+        time_series_all.append([values_str, anm1_mag_fixed, time_anomaly1, anm_mag*sign, time_anomaly2])
 
 
 # Save to CSV
-saved_path = "data/prob_eva_syn_time_series/syn_rsic_simple_ts_gen_lttolt.csv"
+saved_path = "data/prob_eva_syn_time_series/syn_rsic_simple_ts_gen_lttoll.csv"
 df_time_series_all = pd.DataFrame(time_series_all, columns=["values", "anomaly1_magnitude", "anomaly_start_index1", "anomaly2_magnitude", "anomaly_start_index2"])
 
 # Add one column 'timestamp': time_stamps, only for the first row
@@ -157,10 +160,10 @@ for _, row in df.iterrows():
 fig = plt.figure(figsize=(10, 6))
 gs = gridspec.GridSpec(1, 1)
 ax0 = plt.subplot(gs[0])
-# # Randomly plot samples time series
-# random_indices = np.random.choice(len(restored_data), size=2, replace=False)
-# for j in random_indices:
-for j in range(len(restored_data)):
+# Randomly plot samples time series
+random_indices = np.random.choice(len(restored_data), size=2, replace=False)
+for j in random_indices:
+# for j in range(len(restored_data)):
     ax0.plot(time_stamps, restored_data[j][0])
     ax0.axvline(x=restored_data[j][3], color='g', linestyle='--')
     ax0.axvline(x=restored_data[j][4], color='r', linestyle='--')